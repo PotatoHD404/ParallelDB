f93e98776609b812b561c5130a93923059cfe359
Artemii <artem-lukichev@bk.ru>
2022-12-04 23:21:56 +0300

    assets loaded

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,12 +17,14 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
+.app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-image: url('~@/assets/bg.svg');
+  background-size: cover;
   margin-top: 60px;
 }
 </style>

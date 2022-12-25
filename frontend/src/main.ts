5cc9dfa667cab4d497d9c582f732b9040aa4a2d1
Artemii <artem-lukichev@bk.ru>
2022-12-25 23:22:13 +0300

    New error handler

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import MyButton from '@/components/UI/MyButton.vue'
 
 const app = createApp(App)
-app.component('MyButton', MyButton)
 app.mount('#app')

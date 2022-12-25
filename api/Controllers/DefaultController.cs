774913cf7f4ead006378d5543c8ef202a74c6da3
Artemii <artem-lukichev@bk.ru>
2022-12-25 19:11:10 +0300

    Connected backend with frontend

---
+++ api/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+        
+        [HttpPost]
+        public SqlResponse Post(SqlRequest body)
+        {
+            Console.WriteLine(body.Request);
+            var response = new SqlResponse();
+            response.Columns = new []{"Column1", "Column2"};
+            response.Rows = new[] { new dynamic[] { "123", 1 }, new dynamic[] { "123", "2" } };
+            response.SyntaxTree = "dirgraph {a->b}";
+            return response;
+        }
+    }
+}
--- api/Controllers/WeatherForecastController.cs
+++ api/Controllers/WeatherForecastController.cs
@@ -10,7 +10,6 @@ namespace Api.Controllers
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)

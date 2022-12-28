8a0341aeb1064ed5a6a6a9668967afbad045116a
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2022-12-28 20:25:12 +0300

    Small refactor

--- DependencyExample/OperationData.cs
+++ DependencyExample/OperationData.cs
@@ -2,10 +2,19 @@
 
 public class OperationData
 {
-    internal int Id;
-    internal Action Operation;
-    internal int[] Dependencies;
-    internal ExecutionContext Context;
+    internal readonly int Id;
+    internal readonly Action Operation;
+    internal readonly int[] Dependencies;
+    internal readonly ExecutionContext Context;
     internal int NumRemainingDependencies;
     internal DateTimeOffset Start, End;
+    
+    public OperationData(int id, Action operation, int[] dependencies, ExecutionContext? context)
+    {
+        Id = id;
+        Operation = operation;
+        Dependencies = dependencies;
+        Context = context ?? throw new ArgumentNullException(nameof(context));
+        NumRemainingDependencies = dependencies.Length;
+    }
 }

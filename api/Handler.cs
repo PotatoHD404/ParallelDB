b3f1976888173d3551f63aa5d9436522a30c6ba7
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2023-01-02 14:26:27 +0300

    Added all

--- api/Handler.cs
+++ api/Handler.cs
@@ -1,54 +1,42 @@
-﻿using System.Text.Json;
-using Api.Controllers;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace Api
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();   
+builder.Services.AddCors();
+
+
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+
+var app = builder.Build();
+
+// CORS - Allow calling the API from WebBrowsers
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials()
+    //.WithOrigins("https://localhost:44351")); // Allow only this origin can also have multiple origins seperated with comma
+    .SetIsOriginAllowed(origin => true)); // Allow any origin  
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-// lambda handler function for asp.net core should trigger DefaultController
-    public class Handler
-    {
-        public class Request
-        {
-            public string httpMethod { get; set; }
-            public string body { get; set; }
-        }
-
-        private static readonly JsonSerializerSettings JsonSettings = new()
-        {
-            ContractResolver = new DefaultContractResolver
-            {
-                NamingStrategy = new SnakeCaseNamingStrategy()
-            },
-            Formatting = Formatting.Indented
-        };
-
-        private static readonly DefaultController Controller =
-            new(new LoggerFactory().CreateLogger<DefaultController>());
-
-        public string FunctionHandler(Request r)
-        {
-            var method = r.httpMethod;
-
-            switch (method)
-            {
-                case "GET":
-                    return Controller.Get();
-                case "POST":
-                {
-                    var body = r.body;
-                    SqlRequest? jsonDoc = JsonConvert.DeserializeObject<SqlRequest>(body, JsonSettings);
-                    if (jsonDoc == null)
-                    {
-                        return "Invalid request";
-                    }
-
-                    return JsonConvert.SerializeObject(Controller.Post(jsonDoc));
-                }
-                default:
-                    return "Method not supported";
-            }
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+// app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
